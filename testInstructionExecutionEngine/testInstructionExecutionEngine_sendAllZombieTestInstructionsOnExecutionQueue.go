8a5c5473e75c601e714186cf04ae5f7fd1c5d9a3
jlamber <jonas.lambert@drakon.se>
2022-12-08 13:53:06 +0100

    Only move zombie TestInstructionExecutions from Queue when no one of the ongoing failed

--- testInstructionExecutionEngine/testInstructionExecutionEngine_sendAllZombieTestInstructionsOnExecutionQueue.go
+++ testInstructionExecutionEngine/testInstructionExecutionEngine_sendAllZombieTestInstructionsOnExecutionQueue.go
@@ -1,9 +1,12 @@
 package testInstructionExecutionEngine
 
 import (
+	"FenixExecutionServer/common_config"
 	"context"
+	"github.com/jackc/pgx/v4"
 	fenixSyncShared "github.com/jlambert68/FenixSyncShared"
 	"github.com/sirupsen/logrus"
+	"math/rand"
 	"time"
 )
 
@@ -18,9 +21,20 @@ func (executionEngine *TestInstructionExecutionEngineStruct) sendAllZombieTestIn
 		"id": "ecbefc72-f244-4ee5-beb9-e5098842a589",
 	}).Debug("Outgoing 'sendAllZombieTestInstructionsOnExecutionQueue'")
 
+	// Begin SQL Transaction
+	txn, err := fenixSyncShared.DbPool.Begin(context.Background())
+	if err != nil {
+		common_config.Logger.WithFields(logrus.Fields{
+			"id":    "5e76978d-431c-4d7f-a119-9bdee673ae47",
+			"error": err,
+		}).Error("Problem to do 'DbPool.Begin'  in 'prepareInformThatThereAreNewTestCasesOnExecutionQueueSaveToCloudDB'")
+
+		return err
+	}
+
 	// Load all TestCasesExecutions for Zombie-TestInstructions that are stuck in UnderExecutions
 	var testCaseExecutionsToProcess []ChannelCommandTestCaseExecutionStruct
-	testCaseExecutionsToProcess, err = executionEngine.loadAllZombieTestInstructionExecutionsOnExecutionQueue()
+	testCaseExecutionsToProcess, err = executionEngine.loadAllZombieTestInstructionExecutionsOnExecutionQueue(txn)
 	if err != nil {
 		return err
 	}
@@ -43,37 +57,75 @@ func (executionEngine *TestInstructionExecutionEngineStruct) sendAllZombieTestIn
 }
 
 // Load all TestCasesExecutions for Zombie-TestInstructions that are stuck OnExecutionsQueue
-func (executionEngine *TestInstructionExecutionEngineStruct) loadAllZombieTestInstructionExecutionsOnExecutionQueue() (testCaseExecutionsToProcess []ChannelCommandTestCaseExecutionStruct, err error) {
+func (executionEngine *TestInstructionExecutionEngineStruct) loadAllZombieTestInstructionExecutionsOnExecutionQueue(dbTransaction pgx.Tx) (testCaseExecutionsToProcess []ChannelCommandTestCaseExecutionStruct, err error) {
+
+	/*
+		sqlToExecute := ""
+		sqlToExecute = sqlToExecute + "SELECT DISTINCT " +
+			"TIEQ.\"TestCaseExecutionUuid\", TIEQ.\"TestCaseExecutionVersion\", TIEQ.\"QueueTimeStamp\" "
+		sqlToExecute = sqlToExecute + "FROM \"FenixExecution\".\"TestInstructionExecutionQueue\" TIEQ "
+		sqlToExecute = sqlToExecute + "ORDER BY TIEQ.\"QueueTimeStamp\" ASC "
+		sqlToExecute = sqlToExecute + "; "
+	*/
+
+	// Generate unique number for temporary table namne
+	rand.Seed(time.Now().UnixNano())
+	min := 1
+	max := 100000
+	randomNumber := rand.Intn(max-min+1) + min
+	var randomNumberAsString string
+	randomNumberAsString = string(randomNumber)
+	var tempraryTableName = "TEMP_TABLE_" + randomNumberAsString
 
 	sqlToExecute := ""
-	sqlToExecute = sqlToExecute + "SELECT DISTINCT " +
-		"TIEQ.\"TestCaseExecutionUuid\", TIEQ.\"TestCaseExecutionVersion\", TIEQ.\"QueueTimeStamp\" "
-	sqlToExecute = sqlToExecute + "FROM \"FenixExecution\".\"TestInstructionExecutionQueue\" TIEQ "
-	sqlToExecute = sqlToExecute + "ORDER BY TIEQ.\"QueueTimeStamp\" ASC "
-	sqlToExecute = sqlToExecute + "; "
+	sqlToExecute = sqlToExecute + "CREATE TEMPORARY TABLE " + tempraryTableName + " ON COMMIT DROP AS "
+	sqlToExecute = sqlToExecute + "SELECT DISTINCT TIUE.\"TestCaseExecutionUuid\", TIUE.\"TestCaseExecutionVersion\" "
+	sqlToExecute = sqlToExecute + "FROM \"FenixExecution\".\"TestInstructionsUnderExecution\" TIUE "
+	sqlToExecute = sqlToExecute + "GROUP BY TIUE.\"TestCaseExecutionUuid\", TIUE.\"TestCaseExecutionVersion\", TIUE.\"TestInstructionExecutionStatus\" "
+	sqlToExecute = sqlToExecute + "HAVING TIUE.\"TestInstructionExecutionStatus\"  NOT IN (4) " // 4=TestInstructionExecution ended OK
+	sqlToExecute = sqlToExecute + ";"
 
-	SELECT DISTINCT TIUE."TestCaseExecutionUuid", TIUE."TestCaseExecutionVersion"
-	INTO TABLE TEMP_TABLE
-	FROM "FenixExecution"."TestInstructionsUnderExecution" TIUE
-	GROUP BY TIUE."TestCaseExecutionUuid", TIUE."TestCaseExecutionVersion", TIUE."TestInstructionExecutionStatus"
-	HAVING TIUE."TestInstructionExecutionStatus"  NOT IN (4);
-
-	SELECT TIEQ."TestCaseExecutionUuid", TIEQ."TestCaseExecutionVersion", TIEQ."QueueTimeStamp"
-	FROM "FenixExecution"."TestInstructionExecutionQueue" TIEQ
-	LEFT JOIN TEMP_TABLE tmp
-	ON TIEQ."TestCaseExecutionUuid" = tmp."TestCaseExecutionUuid" AND
-	TIEQ."TestCaseExecutionVersion" = tmp."TestCaseExecutionVersion"
-	WHERE tmp."TestCaseExecutionUuid"  IS NULL AND
-	tmp."TestCaseExecutionVersion" IS NULL;
-
-	// Query DB
 	// Execute Query CloudDB
-	//TODO change so we use the dbTransaction instead so rows will be locked ----- comandTag, err := dbTransaction.Exec(context.Background(), sqlToExecute)
-	rows, err := fenixSyncShared.DbPool.Query(context.Background(), sqlToExecute)
+	comandTag, err := dbTransaction.Exec(context.Background(), sqlToExecute)
 
 	if err != nil {
-		executionEngine.logger.WithFields(logrus.Fields{
-			"Id":           "6641053f-8c30-4fd0-872c-ff33d7498588",
+		common_config.Logger.WithFields(logrus.Fields{
+			"Id":           "09356aee-fb7a-4731-8741-d3cfd61844ca",
+			"Error":        err,
+			"sqlToExecute": sqlToExecute,
+		}).Error("Something went wrong when executing SQL")
+
+		return nil, err
+	}
+
+	// Log response from CloudDB
+	common_config.Logger.WithFields(logrus.Fields{
+		"Id":                       "dff4d8fd-3c9b-456f-95b4-fe75a0103a5d",
+		"comandTag.Insert()":       comandTag.Insert(),
+		"comandTag.Delete()":       comandTag.Delete(),
+		"comandTag.Select()":       comandTag.Select(),
+		"comandTag.Update()":       comandTag.Update(),
+		"comandTag.RowsAffected()": comandTag.RowsAffected(),
+		"comandTag.String()":       comandTag.String(),
+	}).Debug("Return data for SQL executed in database")
+
+	sqlToExecute = ""
+	sqlToExecute = sqlToExecute + "SELECT TIEQ.\"TestCaseExecutionUuid\", TIEQ.\"TestCaseExecutionVersion\", " +
+		"TIEQ.\"QueueTimeStamp\" "
+	sqlToExecute = sqlToExecute + "FROM \"FenixExecution\".\"TestInstructionExecutionQueue\" TIEQ "
+	sqlToExecute = sqlToExecute + "LEFT JOIN  " + tempraryTableName + " tmp "
+	sqlToExecute = sqlToExecute + "ON TIEQ.\"TestCaseExecutionUuid\" = tmp.\"TestCaseExecutionUuid\" AND "
+	sqlToExecute = sqlToExecute + "TIEQ.\"TestCaseExecutionVersion\" = tmp.\"TestCaseExecutionVersion\" "
+	sqlToExecute = sqlToExecute + "WHERE tmp.\"TestCaseExecutionUuid\"  IS NULL AND "
+	sqlToExecute = sqlToExecute + "tmp.\"TestCaseExecutionVersion\" IS NULL "
+	sqlToExecute = sqlToExecute + ";"
+
+	// Execute Query CloudDB
+	rows, err := dbTransaction.Query(context.Background(), sqlToExecute)
+
+	if err != nil {
+		common_config.Logger.WithFields(logrus.Fields{
+			"Id":           "934fbad2-5197-4881-82fd-3edc47ce9c50",
 			"Error":        err,
 			"sqlToExecute": sqlToExecute,
 		}).Error("Something went wrong when executing SQL")
